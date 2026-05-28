cba678142930f5600eceebfbee87f99c6d2a2129
Kenan999 <kenanali209@gmail.com>
2026-05-28 09:04:11 +0200

    Integrate OMDb API and add HTML templates

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
 
-from flask import Flask
+import requests
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
 
 from data_manager import DataManager
-from models import db, Movie
+from models import db, Movie, User
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -15,10 +19,77 @@ db.init_app(app)
 
 data_manager = DataManager()
 
+OMDB_API_KEY = os.getenv("OMDB_API_KEY")
+
 
 @app.route("/")
 def home():
-    return "Welcome to MoviWeb App!"
+    users = data_manager.get_users()
+    return render_template("index.html", users=users)
+
+
+@app.route("/users", methods=["POST"])
+def add_user():
+    name = request.form["name"]
+    data_manager.create_user(name)
+    return redirect(url_for("home"))
+
+
+@app.route("/users/<int:user_id>/movies", methods=["GET", "POST"])
+def user_movies(user_id):
+    if request.method == "POST":
+        title = request.form["name"]
+
+        if OMDB_API_KEY:
+            resp = requests.get(
+                "http://www.omdbapi.com/",
+                params={"t": title, "apikey": OMDB_API_KEY},
+            )
+            data = resp.json()
+
+            if data.get("Response") == "True":
+                director = data.get("Director", "")
+                year = data.get("Year", "")
+                try:
+                    year = int(year[:4])
+                except (ValueError, TypeError):
+                    year = None
+                poster_url = data.get("Poster", "")
+                movie = Movie(name=title, director=director, year=year, poster_url=poster_url, user_id=user_id)
+            else:
+                movie = Movie(name=title, user_id=user_id)
+        else:
+            movie = Movie(name=title, user_id=user_id)
+
+        data_manager.add_movie(movie)
+        return redirect(url_for("user_movies", user_id=user_id))
+
+    movies = data_manager.get_movies(user_id)
+    user = User.query.get(user_id)
+    return render_template("movies.html", movies=movies, user=user)
+
+
+@app.route("/users/<int:user_id>/movies/<int:movie_id>/update", methods=["POST"])
+def update_movie(user_id, movie_id):
+    new_title = request.form["name"]
+    data_manager.update_movie(movie_id, new_title)
+    return redirect(url_for("user_movies", user_id=user_id))
+
+
+@app.route("/users/<int:user_id>/movies/<int:movie_id>/delete", methods=["POST"])
+def delete_movie(user_id, movie_id):
+    data_manager.delete_movie(movie_id)
+    return redirect(url_for("user_movies", user_id=user_id))
+
+
+@app.route("/docs")
+def docs():
+    return send_from_directory(basedir, "docs.html")
+
+
+@app.route("/openapi.yaml")
+def openapi_spec():
+    return send_from_directory(basedir, "openapi.yaml")
 
 
 if __name__ == "__main__":
