73c916527d7869180fdeeb77fae34f9b2a521e6f
Kenan999 <kenanali209@gmail.com>
2026-05-28 04:41:48 +0200

    Add database models and CRUD manager

---
+++ data_manager.py
@@ -0,0 +1,41 @@
+from models import db, User, Movie
+
+
+class DataManager:
+
+    @staticmethod
+    def create_user(name):
+        user = User(name=name)
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    @staticmethod
+    def get_users():
+        return User.query.all()
+
+    @staticmethod
+    def get_movies(user_id):
+        return Movie.query.filter_by(user_id=user_id).all()
+
+    @staticmethod
+    def add_movie(movie):
+        db.session.add(movie)
+        db.session.commit()
+        return movie
+
+    @staticmethod
+    def update_movie(movie_id, new_title):
+        movie = Movie.query.get(movie_id)
+        if movie:
+            movie.name = new_title
+            db.session.commit()
+        return movie
+
+    @staticmethod
+    def delete_movie(movie_id):
+        movie = Movie.query.get(movie_id)
+        if movie:
+            db.session.delete(movie)
+            db.session.commit()
+        return movie
